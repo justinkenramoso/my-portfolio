97b57392810aec8090244d16c071d2429c23556a
justinkenramoso <justinken.ramoso@gmail.com>
2022-12-09 15:13:07 +0800

    Fixed skills flexbox

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -5,10 +5,10 @@ function SkillCard(props) {
     <div className="col">
       <div className={`skill-card p-3 ${props.logo}`}>
         <div className="row">
-          <div className="col-12 col-md-6 d-flex justify-content-center  align-items-center">
+          <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
             <i className={`fa-4x fa-brands fa-${props.logo}`}></i>
           </div>
-          <div className="d-none d-md-block col-md-6 d-flex justify-content-start align-items-center">
+          <div className="d-none d-md-flex col-md-6 d-flex justify-content-start align-items-center">
             <h4 className={`skill-name m-0`}>{props.name}</h4>
           </div>
         </div>
